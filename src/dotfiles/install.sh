a2c243735b17c9595fbce40291b1f8677bbf5edb
Kyle Robertson <kyle@kairsaerospace.com>
2025-03-05 15:37:21 -0700

    fix script

--- src/dotfiles/install.sh
+++ src/dotfiles/install.sh
@@ -17,8 +17,6 @@ echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_
 
 echo "The effective dev container containerUser is '$_CONTAINER_USER'"
 echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
-echo "Contents of .ssh directory:"
-ls -alth $_CONTAINER_USER_HOME/.ssh/
 
 # Install the YADM dotfiles manager
 curl -fLo /usr/local/bin/yadm https://github.com/yadm-dev/yadm/raw/master/yadm
